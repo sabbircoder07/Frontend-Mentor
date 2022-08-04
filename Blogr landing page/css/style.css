b4a4b51c0c188f76dedd5a0645f9720a7fc27845
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-08-04 19:22:05 +0600

    Update style.css

--- Blogr landing page/css/style.css
+++ Blogr landing page/css/style.css
@@ -1,42 +1,424 @@
-const allMenu = document.querySelectorAll(".dropdown");
-const arrowSelection = document.querySelectorAll(".arrow");
-const navLink = document.querySelectorAll(".nav-link");
-console.log(navLink);
+/*
+# Front-end Style Guide
 
-function handleOnClick(event) {
-  event.stopPropagation();
-  event.preventDefault();
-  const classList = event.currentTarget.classList;
-  const isOpen = classList.contains("dropdown--open");
-  const classListLink = event.currentTarget.classList;
-  const isActiveLink = classList.contains("nav-link-extend");
+## Layout
 
-  allMenu.forEach(function (element) {
-    element.classList.remove("nav-link-extend");
-    element.classList.remove("dropdown--open");
-  });
+The designs were created to the following widths:
 
-  if (isActiveLink) {
-    classListLink.remove("nav-link-extend");
-  } else {
-    classListLink.add("nav-link-extend");
-  }
-  if (isOpen) {
-    classList.remove("dropdown--open");
-  } else {
-    classList.add("dropdown--open");
-  }
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Light red (CTA text): hsl(356, 100%, 66%)
+- Very light red (CTA hover background): hsl(355, 100%, 74%)
+- Very dark blue (headings): hsl(208, 49%, 24%)
+
+### Neutral
+
+- White (text): hsl(0, 0%, 100%)
+- Grayish blue (footer text): hsl(240, 2%, 79%)
+- Very dark grayish blue (body copy): hsl(207, 13%, 34%)
+- Very dark black blue (footer background): hsl(240, 10%, 16%)
+
+### Gradient
+
+Background gradient - Intro/CTA mobile nav:
+
+- Very light red: hsl(13, 100%, 72%)
+- Light red: hsl(353, 100%, 62%)
+
+Background gradient - body:
+
+- Very dark gray blue: hsl(237, 17%, 21%)
+- Very dark desaturated blue: hsl(237, 23%, 32%)
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Fonts
+
+- Family: [Overpass](https://fonts.google.com/specimen/Overpass?preview.text_type=custom)
+- Weights: 300, 600
+
+- Family: [Ubuntu](https://fonts.google.com/specimen/Ubuntu?preview.text_type=custom)
+- Weights: 400, 500, 700
+
+*/
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
-allMenu.forEach((element) => element.addEventListener("click", handleOnClick));
-document.body.addEventListener("click", function (event) {
-  allMenu.forEach(function (element) {
-    if (
-      element.classList.contains("dropdown--open") &&
-      element.classList.contains("nav-link-extend")
-    ) {
-      element.classList.remove("nav-link-extend");
-      element.classList.remove("dropdown--open");
-    }
-  });
-});
+html {
+  font-size: 62.5% !important;
+}
+
+body {
+  font-family: "Overpass", sans-serif;
+  line-height: 1;
+  font-weight: 400;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 700;
+}
+a {
+  text-decoration: none;
+}
+
+.container {
+  max-width: 144rem;
+  margin: 0 auto;
+  overflow: hidden;
+}
+
+.hero-section {
+  background-image: linear-gradient(
+      to right,
+      rgba(255, 143, 112, 0.85),
+      rgba(255, 61, 84, 0.85)
+    ),
+    url("../images/bg-pattern-intro-desktop.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 0 0 0 10rem;
+}
+
+.hero-header-section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  justify-items: center;
+  height: 15rem;
+  padding: 0 15rem;
+  color: hsl(0, 0%, 100%);
+}
+
+.blog-logo {
+  width: 100%;
+  height: auto;
+  cursor: pointer;
+}
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3.8rem;
+}
+
+.nav-list {
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3.2rem;
+}
+
+.nav-link {
+  display: block;
+  font-size: 1.6rem;
+  color: hsl(0, 0%, 100%);
+}
+
+.nav-link-extend {
+  text-decoration: underline;
+}
+
+.arrow {
+  padding-left: 5px;
+}
+
+.dropdown {
+  position: relative;
+  padding-right: 0.5rem;
+}
+
+.dropdown-menu {
+  display: none;
+}
+
+.dropdown--open .dropdown-menu {
+  display: block;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background: #fff;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  min-width: 100%;
+  width: 15rem;
+  transition: all 0.4s;
+}
+
+.dropdown-item {
+  font-size: 1.6rem;
+  display: block;
+  text-decoration: none;
+  color: hsl(208, 49%, 24%);
+  padding: 5px;
+  transition: all 0.4s;
+}
+
+.dropdown-item:hover,
+.dropdown-item:active {
+  font-size: 1.6rem;
+  font-weight: 700;
+  color: hsl(208, 49%, 24%);
+  background-color: #f7f7f7;
+}
+
+.nav-cta {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+.login-option:link,
+.login-option:visited {
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: hsl(0, 0%, 100%);
+}
+
+.sign-up-btn {
+  border: none;
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: hsl(356, 100%, 66%);
+  background-color: hsl(0, 0%, 100%);
+  padding: 1.2rem 2.2rem 1rem 2.2rem;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: all 0.4s;
+}
+
+.sign-up-btn:hover {
+  color: hsl(0, 0%, 100%);
+  background-color: hsl(355, 100%, 74%);
+}
+
+.hero-description {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10rem 0;
+}
+
+.hero-header {
+  font-size: 5.8rem;
+  font-weight: 700;
+  color: hsl(0, 0%, 100%);
+  margin-bottom: 2.8rem;
+}
+.hero-text {
+  font-size: 1.6rem;
+  color: hsl(0, 0%, 100%);
+  margin-bottom: 4.8rem;
+}
+.hero-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.8rem;
+  margin-bottom: 10rem;
+}
+.for-free-btn:link,
+.for-free-btn:visited {
+  text-align: center;
+  border: none;
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: hsl(356, 100%, 66%);
+  background-color: hsl(0, 0%, 100%);
+  padding: 1.4rem 2.2rem 1rem 2.2rem;
+  border-radius: 25px;
+  transition: all 0.4s;
+}
+
+.for-free-btn:hover,
+.for-free-btn:active {
+  color: hsl(0, 0%, 100%);
+  background-color: hsl(355, 100%, 74%);
+}
+
+.learn-more-btn:link,
+.learn-more-btn:visited {
+  border: 2px solid hsl(240, 2%, 79%);
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: hsl(0, 0%, 100%);
+  background-color: transparent;
+  padding: 1.4rem 2.2rem 1rem 2.2rem;
+  border-radius: 25px;
+  transition: all 0.4s;
+}
+
+.learn-more-btn:hover,
+.learn-more-btn:active {
+  color: hsl(356, 100%, 66%);
+  background-color: hsl(0, 0%, 100%);
+}
+.feature-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  margin-top: 15rem;
+  margin-bottom: 15rem;
+}
+.feature-header {
+  font-size: 4.4rem;
+  font-weight: 700;
+  color: hsl(208, 49%, 24%);
+}
+
+.feature-details {
+  display: flex;
+  flex-direction: row;
+}
+
+.feature-descriptions {
+  display: flex;
+  flex-direction: column;
+  gap: 4.8rem;
+  padding: 10rem 20rem 10rem 15rem;
+  width: 100%;
+}
+.feature-description-header {
+  font-size: 2.8rem;
+  font-weight: 500;
+  color: hsl(208, 49%, 24%);
+  margin-bottom: 2.8rem;
+}
+.feature-description-text {
+  font-size: 1.6rem;
+  line-height: 1.6;
+  color: hsl(208, 49%, 24%);
+  margin-bottom: 2.8rem;
+}
+.feature-image {
+  background-image: url("../images/illustration-editor-desktop.svg");
+  background-repeat: no-repeat;
+  background-position: left;
+  background-size: cover;
+  width: 74%;
+  height: auto;
+  transform: scale(1.3);
+}
+
+.infrastructure-section {
+  background-image: url("../images/bg-pattern-circles.svg");
+  background-color: hsl(237, 17%, 21%);
+  display: flex;
+  background-repeat: no-repeat;
+  background-position: -49% 85%;
+  background-size: 70%;
+  margin-top: 15rem;
+  margin-bottom: 15rem;
+  border-radius: 0 10rem 0 10rem;
+}
+
+.infrastructure-image {
+  background-image: url("../images/illustration-phones.svg");
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: contain;
+  width: 100%;
+  height: auto;
+  transform: scale(1.5);
+}
+
+.infrastructure-descripyon {
+  padding: 15rem 15rem 10rem 20rem;
+}
+.feature-description-header-extend,
+.feature-description-text-extend {
+  color: hsl(0, 0%, 100%) !important ;
+}
+.feature-image-extend {
+  background-image: url("../images/illustration-laptop-desktop.svg");
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: cover;
+  width: 100%;
+  height: auto;
+  transform: scale(1.3);
+}
+
+.footer {
+  background-color: hsl(240, 10%, 16%);
+  padding: 8rem 5rem 8rem 5rem;
+  border-radius: 0 10rem 0 0;
+  margin-top: 15rem;
+}
+
+.footer-setcion {
+  display: flex;
+  justify-content: space-around;
+  gap: 2.4rem;
+}
+
+.blog-logo-footer {
+  width: 30%;
+  height: auto;
+  margin-bottom: 2.8rem;
+}
+
+.social-icon {
+  display: flex;
+  text-align: bottom;
+  justify-content: space-between;
+  gap: 1rem;
+}
+.fa {
+  padding: 1rem;
+  font-size: 1.6rem;
+  text-align: center;
+  text-decoration: none;
+  color: white;
+  background: hsl(356, 100%, 66%);
+  border-radius: 5px;
+  transition: all 0.3s;
+}
+
+/* Add a hover effect if you want */
+.fa:hover {
+  color: hsl(300, 69%, 71%);
+}
+.menu-header {
+  font-size: 1.8rem;
+  color: hsl(0, 0%, 100%);
+  margin-bottom: 2.4rem;
+}
+
+.menu-list {
+  font-size: 1.6rem;
+  color: hsl(240, 2%, 79%);
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 1.8rem;
+}
+
+.nav-link-footer {
+  display: block;
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: hsl(240, 2%, 79%);
+}
