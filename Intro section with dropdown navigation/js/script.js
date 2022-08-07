dfeebeb3fdf70cedc717a70fa5b14d96e2428c4c
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-08-07 10:34:10 +0600

    Update script.js

--- Intro section with dropdown navigation/js/script.js
+++ Intro section with dropdown navigation/js/script.js
@@ -2,11 +2,17 @@ const allMenu = document.querySelectorAll(".dropdown");
 const arrowSelection = document.querySelectorAll(".arrow");
 const navLink = document.querySelectorAll(".nav-link");
 
+console.log(arrowSelection);
+
 function handleOnClick(event) {
   event.stopPropagation();
   event.preventDefault();
   const classList = event.currentTarget.classList;
   const isOpen = classList.contains("dropdown--open");
+
+  arrowSelection.forEach(function (element) {
+    element.src = "./images/icon-arrow-down.svg";
+  });
 
   allMenu.forEach(function (element) {
     element.classList.remove("dropdown--open");
@@ -14,13 +20,21 @@ function handleOnClick(event) {
 
   if (isOpen) {
     classList.remove("dropdown--open");
+    event.currentTarget.querySelector(".arrow").src =
+      "./images/icon-arrow-down.svg";
   } else {
     classList.add("dropdown--open");
+    event.currentTarget.querySelector(".arrow").src =
+      "./images/icon-arrow-up.svg";
   }
 }
 
 allMenu.forEach((element) => element.addEventListener("click", handleOnClick));
 document.body.addEventListener("click", function (event) {
+  arrowSelection.forEach(function (element) {
+    element.src = "./images/icon-arrow-down.svg";
+  });
+
   allMenu.forEach(function (element) {
     if (element.classList.contains("dropdown--open")) {
       element.classList.remove("dropdown--open");
