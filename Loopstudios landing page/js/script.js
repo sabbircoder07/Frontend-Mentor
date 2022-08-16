ab7f4dcf68bec641968e8cc8900706daa8b3437d
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-08-16 14:24:00 +0600

    Update script.js

--- Loopstudios landing page/js/script.js
+++ Loopstudios landing page/js/script.js
@@ -1,1 +1,24 @@
+const btnMobileNav = document.querySelector(".btn-mobile-nav");
+const navBar = document.querySelector(".nav-bar");
+
+function handleOnClick(event) {
+  event.stopPropagation();
+  event.preventDefault();
+
+  const isOpen = navBar.classList.contains("nav--open");
+  navBar.classList.remove("nav--open");
+
+  if (isOpen) {
+    navBar.classList.remove("nav--open");
+  } else {
+    navBar.classList.add("nav--open");
+  }
+}
+
+btnMobileNav.addEventListener("click", handleOnClick);
+document
+  .querySelector(".mobile-nav-icon-close")
+  .addEventListener("click", function (event) {
+    navBar.classList.remove("nav--open");
+  });
 
