5efb3487630e07ca64e7eb79dac207c7cbdfa350
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-08-19 03:49:44 +0600

    Update script.js

--- Loopstudios landing page/js/script.js
+++ Loopstudios landing page/js/script.js
@@ -1,5 +1,28 @@
 const btnMobileNav = document.querySelector(".btn-mobile-nav");
 const navBar = document.querySelector(".nav-bar");
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+  if (link.classList.contains("nav-link-header")) {
+    navBar.classList.toggle("nav--open");
+  }
+});
+
+document.querySelector(".current-year").textContent = new Date().getFullYear();
 
 function handleOnClick(event) {
   event.stopPropagation();
@@ -21,4 +44,3 @@ document
   .addEventListener("click", function (event) {
     navBar.classList.remove("nav--open");
   });
-
