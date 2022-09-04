d7cf93f3d935f1ce4a7b13f61f3b1fc9979eaf3c
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-04 13:13:51 +0600

    Update script.js

--- Ping single column coming soon page/js/script.js
+++ Ping single column coming soon page/js/script.js
@@ -1,7 +1,12 @@
 const submitBtn = document.querySelector(".btn-submit");
 const errorIcon = document.querySelector(".error-icon");
+const validIcon = document.querySelector(".fa-check");
 const errorMessage = document.querySelector(".error-message");
 const emailID = document.querySelector("#email-id");
+
+const copyrightYear = document.querySelector(".year");
+
+copyrightYear.innerHTML = new Date().getFullYear();
 
 function isValidEmail(email) {
   const rex =
@@ -11,17 +16,29 @@ function isValidEmail(email) {
     errorIcon.style.display = "none";
     emailID.classList.remove("invalid-email-id");
     errorMessage.innerHTML = "";
+    validIcon.style.display = "block";
   } else {
     emailID.classList.add("invalid-email-id");
     emailID.focus();
     errorIcon.style.display = "block";
     errorMessage.style.color = "#c92a2a";
-    errorMessage.innerHTML = "Please provide a valid email address";
+    errorMessage.innerHTML = "Please provide a valid Email Address";
+    validIcon.style.display = "none";
   }
 }
-function handleToClick() {
+function handleToClick(event) {
+  event.stopPropagation();
+  event.preventDefault();
   isValidEmail(emailID.value);
 }
 submitBtn.addEventListener("click", handleToClick);
-emailID.addEventListener("focusout", handleToClick);
-emailID.addEventListener("change", handleToClick);
+emailID.addEventListener("focusout", function () {
+  if (errorMessage.innerHTML != "") {
+    isValidEmail(emailID.value);
+  }
+});
+emailID.addEventListener("change", function () {
+  if (errorMessage.innerHTML != "") {
+    isValidEmail(emailID.value);
+  }
+});
